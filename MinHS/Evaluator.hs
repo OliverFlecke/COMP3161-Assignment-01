92716dfd561184859cb4ceff820f3dcda964e462
Oliver Fleckenstein <oliverfl@live.dk>
2016-09-01 00:54:53 +1000

    Origin commit

---
+++ MinHS/Evaluator.hs
@@ -0,0 +1,29 @@
+module MinHS.Evaluator where
+import qualified MinHS.Env as E
+import MinHS.Syntax
+import MinHS.Pretty
+import qualified Text.PrettyPrint.ANSI.Leijen as PP
+
+type VEnv = E.Env Value
+
+data Value = I Integer
+           | B Bool
+           | Nil
+           | Cons Integer Value
+           -- Others as needed
+           deriving (Show)
+
+instance PP.Pretty Value where
+  pretty (I i) = numeric $ i
+  pretty (B b) = datacon $ show b
+  pretty (Nil) = datacon "Nil"
+  pretty (Cons x v) = PP.parens (datacon "Cons" PP.<+> numeric x PP.<+> PP.pretty v)
+  pretty _ = undefined -- should not ever be used
+
+evaluate :: Program -> Value
+evaluate [Bind _ _ _ e] = evalE E.empty e
+evaluate bs = evalE E.empty (Let bs (Var "main"))
+
+
+evalE :: VEnv -> Exp -> Value
+evalE g e = error "Implement me!"
